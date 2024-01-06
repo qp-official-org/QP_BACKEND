e8b9148431e70fbf2a0ae248d2f9ef651443edd5
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-01-06 18:25:59 +0900

    init commit

---
+++ src/main/java/qp/official/qp/QpBackendApplication.java
@@ -0,0 +1,13 @@
+package qp.official.qp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/qp/official/qp/QpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package qp.official.qp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
