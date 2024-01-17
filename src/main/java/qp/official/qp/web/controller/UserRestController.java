15c6996b8e2ecad73407df84f2c88bc32307fe91
joon <yijiyoon06@gmail.com>
2024-01-17 13:19:47 +0900

    api 시그니처 생성

--- src/main/java/qp/official/qp/web/controller/UserRestController.java
+++ src/main/java/qp/official/qp/web/controller/UserRestController.java
@@ -1,2 +1,59 @@
-package qp.official.qp.web.controller;public class UserRestController {
+package qp.official.qp.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import qp.official.qp.apiPayload.ApiResponse;
+import qp.official.qp.apiPayload.code.status.SuccessStatus;
+import qp.official.qp.converter.QuestionConverter;
+import qp.official.qp.converter.UserConverter;
+import qp.official.qp.web.dto.QuestionResponseDTO;
+import qp.official.qp.web.dto.UserResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@RequestMapping("/users")
+public class UserRestController {
+
+    @PostMapping("/test")
+    public ApiResponse<UserResponseDTO.UserTestDTO> testAPI(){
+        return ApiResponse.onSuccess(SuccessStatus.Question_OK.getCode(), SuccessStatus.Question_OK.getMessage(), UserConverter.toUserTestDTO());
+    }
+
+    @PostMapping("/sign_up")
+    public ApiResponse<UserResponseDTO.UserTestDTO> signUp(){
+        return ApiResponse.onSuccess(SuccessStatus.Question_OK.getCode(), SuccessStatus.Question_OK.getMessage(), UserConverter.toUserTestDTO());
+    }
+
+    @PostMapping("/sign_in")
+    public ApiResponse<UserResponseDTO.UserTestDTO> signIn(){
+        return ApiResponse.onSuccess(SuccessStatus.Question_OK.getCode(), SuccessStatus.Question_OK.getMessage(), UserConverter.toUserTestDTO());
+    }
+
+    @PostMapping("/{userId}")
+    public ApiResponse<UserResponseDTO.UserTestDTO> getUserInfo(){
+        return ApiResponse.onSuccess(SuccessStatus.Question_OK.getCode(), SuccessStatus.Question_OK.getMessage(), UserConverter.toUserTestDTO());
+
+    }
+
+    @PatchMapping("/{userId}")
+    public ApiResponse<UserResponseDTO.UserTestDTO> updateUserInfo(){
+        return ApiResponse.onSuccess(SuccessStatus.Question_OK.getCode(), SuccessStatus.Question_OK.getMessage(), UserConverter.toUserTestDTO());
+    }
+
+    @PatchMapping("/sign_out")
+    public ApiResponse<UserResponseDTO.UserTestDTO> signOut(){
+        return ApiResponse.onSuccess(SuccessStatus.Question_OK.getCode(), SuccessStatus.Question_OK.getMessage(), UserConverter.toUserTestDTO());
+    }
+
+    @PostMapping("/auto_sign_in")
+    public ApiResponse<UserResponseDTO.UserTestDTO> autoSignIn(){
+        return ApiResponse.onSuccess(SuccessStatus.Question_OK.getCode(), SuccessStatus.Question_OK.getMessage(), UserConverter.toUserTestDTO());
+    }
+
+
 }
