eb6cf28d00be1f133f11506b6182c1fbc1f9fc3b
Minseok Kim <minseokkim@Minseokui-MacBookPro-2.local>
2024-01-17 13:31:40 +0900

    [feat] 질문 기능 API 시그니처 작성

--- src/main/java/qp/official/qp/converter/QuestionConverter.java
+++ src/main/java/qp/official/qp/converter/QuestionConverter.java
@@ -1,11 +1,52 @@
 package qp.official.qp.converter;
 
+import qp.official.qp.domain.Question;
+import qp.official.qp.web.dto.QuestionRequestDTO;
 import qp.official.qp.web.dto.QuestionResponseDTO;
+import qp.official.qp.web.dto.QuestionResponseDTO.QuestionUpdateReturnDTO;
 
 public class QuestionConverter {
-    public static QuestionResponseDTO.QuestionTestDTO toQuestionTestDTO(){
-        return QuestionResponseDTO.QuestionTestDTO.builder()
-                .testString("This is Test!")
-                .build();
+
+    public static Question toQuestion(QuestionRequestDTO.QuestionPostDTO request){
+        return null;
     }
+
+    public static Question toUpdateQuestion(QuestionRequestDTO.QuestionUpdateDTO request){
+        return null;
+    }
+
+    public static QuestionResponseDTO.QuestionReturnDTO toQuestionReturnDTO(Question question){
+        return QuestionResponseDTO.QuestionReturnDTO.builder()
+            .questionId(question.getQuestionId())
+            .createdAt(question.getCreatedAt())
+            .build();
+    }
+
+    public static QuestionResponseDTO.QuestionFindReturnDTO toQuestionFindReturnDTO(Question question){
+        return QuestionResponseDTO.QuestionFindReturnDTO.builder()
+            .questionId(question.getQuestionId())
+            .title(question.getTitle())
+            .content(question.getContent())
+            .hashtags(question.getQuestionHashTagList())
+            .createdAt(question.getCreatedAt())
+            .updatedAt(question.getUpdatedAt())
+            .build();
+    }
+
+    public static QuestionResponseDTO.QuestionPagingTitleReturnDTO toQuestionFindByPagingReturnDTO(){
+        return null;
+    }
+
+    public static QuestionResponseDTO.QuestionUpdateReturnDTO toQuestionUpdateReturnDTO(Question question){
+        return QuestionUpdateReturnDTO.builder()
+            .questionId(question.getQuestionId())
+            .title(question.getTitle())
+            .content(question.getContent())
+            .updatedAt(question.getUpdatedAt())
+            .build();
+    }
+
+
+
+
 }
--- src/main/java/qp/official/qp/web/dto/QuestionResponseDTO.java
+++ src/main/java/qp/official/qp/web/dto/QuestionResponseDTO.java
@@ -1,17 +1,74 @@
 package qp.official.qp.web.dto;
 
+import io.swagger.models.auth.In;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import qp.official.qp.domain.Hashtag;
+import qp.official.qp.domain.mapping.QuestionHashTag;
 
 public class QuestionResponseDTO {
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class QuestionDTO{
+        Long questionId;
+        Long userId;
+        String profileImage;
+        String title;
+        Integer hit;
+        Integer answerCount;
+        Integer expertCount;
+        List<QuestionHashTag> hashtags;
+    }
 
     @Builder
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class QuestionTestDTO{
-        String testString;
+    public static class QuestionReturnDTO{
+        Long questionId;
+        LocalDateTime createdAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class QuestionFindReturnDTO{
+        Long questionId;
+        String title;
+        String content;
+        List<QuestionHashTag> hashtags;
+        LocalDateTime createdAt;
+        LocalDateTime updatedAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class QuestionPagingTitleReturnDTO{
+        List<QuestionDTO> questions;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        boolean isFirst;
+        boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class QuestionUpdateReturnDTO{
+        Long questionId;
+        String title;
+        String content;
+        LocalDateTime updatedAt;
     }
 }
