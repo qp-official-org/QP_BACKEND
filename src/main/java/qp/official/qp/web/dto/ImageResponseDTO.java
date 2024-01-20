f1c0c8e36fb85af3d8cd918cbc6195deff18ed74
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-01-20 20:44:46 +0900

    [feat] 이미지 등록, 삭제 시그니쳐 구현

--- src/main/java/qp/official/qp/web/dto/ImageResponseDTO.java
+++ src/main/java/qp/official/qp/web/dto/ImageResponseDTO.java
@@ -1,4 +1,16 @@
 package qp.official.qp.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class ImageResponseDTO {
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class CreateResultDTO{
+        private String url;
+    }
 }
