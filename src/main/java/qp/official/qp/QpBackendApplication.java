a1689cf609755b6d8add93b8933047fcd8fd1de7
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-01-21 18:48:30 +0900

    [chore] @EnableJpaAuditing 추가

--- src/main/java/qp/official/qp/QpBackendApplication.java
+++ src/main/java/qp/official/qp/QpBackendApplication.java
@@ -2,8 +2,10 @@ package qp.official.qp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class QpBackendApplication {
 
     public static void main(String[] args) {
