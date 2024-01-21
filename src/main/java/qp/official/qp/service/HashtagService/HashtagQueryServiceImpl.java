51d45175b8794fbf97395dfd276ef36605d4e72e
Minseok Kim <minseokkim@Minseokui-MacBookPro-2.local>
2024-01-21 23:51:10 +0900

    [feat] 해시태그 컨버터 수정 및 서비스 Query와 Command로 분리

--- src/main/java/qp/official/qp/service/HashtagService/HashtagQueryServiceImpl.java
+++ src/main/java/qp/official/qp/service/HashtagService/HashtagQueryServiceImpl.java
@@ -1,10 +1,8 @@
 package qp.official.qp.service.HashtagService;
 
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.webjars.NotFoundException;
 import qp.official.qp.apiPayload.code.status.ErrorStatus;
 import qp.official.qp.apiPayload.exception.handler.HashtagHandler;
 import qp.official.qp.converter.HashtagConverter;
@@ -15,13 +13,13 @@ import qp.official.qp.web.dto.HashtagRequestDTO.HashtagDTO;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class HashtagServiceImpl implements HashtagService {
+public class HashtagQueryServiceImpl implements HashtagQueryService {
 
     private final HashtagRepository hashtagRepository;
 
     private void checkIsEmpty(HashtagDTO request){
         if (request.getHashtag() == null || request.getHashtag().isEmpty()) {
-            throw new HashtagHandler(ErrorStatus.HASHTAG_BAD_REQUEST);
+            throw new HashtagHandler(ErrorStatus.HASHTAG_NOT_EXIST);
         }
     }
 
@@ -37,26 +35,11 @@ public class HashtagServiceImpl implements HashtagService {
         return hashtagRepository.save(hashtag);
     }
 
-    @Override
-    public Hashtag findHashtag(HashtagDTO request) {
-        checkIsEmpty(request);
-
-        if (!hashtagRepository.existsByHashtag(request.getHashtag())){
-            throw new HashtagHandler(ErrorStatus.HASHTAG_NOT_EXIST);
-        }
-        return hashtagRepository.findByHashtag(request.getHashtag());
-    }
 
     @Override
-    public Hashtag deleteHashtag(HashtagDTO request, Long hashtagId){
-
-        checkIsEmpty(request);
-
+    public Hashtag deleteHashtag(Long hashtagId){
         Hashtag deleteHashtag = hashtagRepository.findById(hashtagId)
             .orElseThrow(() -> new HashtagHandler(ErrorStatus.HASHTAG_NOT_EXIST));
-        if (!deleteHashtag.getHashtag().equals(request.getHashtag())) {
-            throw new HashtagHandler(ErrorStatus.HASHTAG_NOT_EXIST);
-        }
         hashtagRepository.deleteById(hashtagId);
         return deleteHashtag;
     }
--- src/main/java/qp/official/qp/web/controller/HashtagController.java
+++ src/main/java/qp/official/qp/web/controller/HashtagController.java
@@ -2,7 +2,6 @@ package qp.official.qp.web.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,8 @@ import qp.official.qp.apiPayload.code.status.ErrorStatus;
 import qp.official.qp.apiPayload.code.status.SuccessStatus;
 import qp.official.qp.converter.HashtagConverter;
 import qp.official.qp.domain.Hashtag;
-import qp.official.qp.service.HashtagService.HashtagService;
+import qp.official.qp.service.HashtagService.HashtagCommandService;
+import qp.official.qp.service.HashtagService.HashtagQueryService;
 import qp.official.qp.web.dto.HashtagRequestDTO;
 import qp.official.qp.web.dto.HashtagResponseDTO;
 
@@ -26,24 +26,26 @@ import qp.official.qp.web.dto.HashtagResponseDTO;
 @RequestMapping("/hashtag")
 public class HashtagController {
 
-    private final HashtagService hashtagService;
+    private final HashtagQueryService hashtagQueryService;
+    private final HashtagCommandService hashtagCommandService;
+
 
     @PostMapping("/")
     public ApiResponse<HashtagResponseDTO.HashtagReturnDTO> generate(@RequestBody HashtagRequestDTO.HashtagDTO request){
-        Hashtag hashtag = hashtagService.saveHashtag(request);
+        Hashtag hashtag = hashtagQueryService.saveHashtag(request);
         return ApiResponse.onSuccess(SuccessStatus.Hashtag_OK.getCode(), SuccessStatus.Hashtag_OK.getMessage(), HashtagConverter.toHashtagResultDTO(hashtag));
     }
 
     @GetMapping("/")
     public ApiResponse<HashtagResponseDTO.HashtagReturnDTO> findHashtag(@RequestBody HashtagRequestDTO.HashtagDTO request){
-        Hashtag hashtag = hashtagService.findHashtag(request);
+        Hashtag hashtag = hashtagCommandService.findHashtag(request);
         return ApiResponse.onSuccess(SuccessStatus.Hashtag_OK.getCode(), SuccessStatus.Hashtag_OK.getMessage(), HashtagConverter.toHashtagResultDTO(hashtag));
     }
 
     @DeleteMapping("/{hashtagId}")
-    public ApiResponse<HashtagResponseDTO.HashtagReturnDTO> deleteHashtag(@RequestBody HashtagRequestDTO.HashtagDTO request, @PathVariable Long hashtagId){
+    public ApiResponse<HashtagResponseDTO.HashtagReturnDTO> deleteHashtag(@PathVariable Long hashtagId){
         try {
-            Hashtag hashtag = hashtagService.deleteHashtag(request,hashtagId);
+            Hashtag hashtag = hashtagQueryService.deleteHashtag(hashtagId);
             return ApiResponse.onSuccess(SuccessStatus.Hashtag_OK.getCode(), SuccessStatus.Hashtag_OK.getMessage(), HashtagConverter.toHashtagResultDTO(hashtag));
         } catch (Exception e) {
             return ApiResponse.onFailure(ErrorStatus.HASHTAG_NOT_EXIST.getCode(), ErrorStatus.HASHTAG_NOT_EXIST.getMessage(), null);
