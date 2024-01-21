55c484ac965f5c205716d9d56daa129924ed2d9a
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-01-21 18:43:47 +0900

    [feat] QuestionQueryServiceImpl Transaction 추가

--- src/main/java/qp/official/qp/service/QuestionService/QuestionQueryServiceImpl.java
+++ src/main/java/qp/official/qp/service/QuestionService/QuestionQueryServiceImpl.java
@@ -2,11 +2,13 @@ package qp.official.qp.service.QuestionService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import qp.official.qp.domain.Question;
 import qp.official.qp.repository.QuestionRepository;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class QuestionQueryServiceImpl implements QuestionQueryService{
     private final QuestionRepository questionRepository;
     @Override
