f76ddc29d1d67a57884d2fb988603498ae13388c
이원찬 <dnjscks4257@naver.com>
2024-01-23 11:24:50 +0900

    [feat] 테스트 유저 생성 기능 구현

--- src/main/java/qp/official/qp/converter/UserConverter.java
+++ src/main/java/qp/official/qp/converter/UserConverter.java
@@ -7,25 +7,32 @@ import qp.official.qp.web.dto.UserResponseDTO;
 
 public class UserConverter {
 
-    public static UserResponseDTO.JoinResultDTO toUserTestDTO(){
+    public static UserResponseDTO.JoinResultDTO toUserTestDTO() {
         return UserResponseDTO.JoinResultDTO.builder()
                 .userId(1L)
                 .build();
     }
 
-    public static UserResponseDTO.LoginResultDTO toUserLoginDTO(){
+    public static UserResponseDTO.JoinResultDTO createTestUser(User user) {
+        return UserResponseDTO.JoinResultDTO.builder()
+                .userId(user.getUserId())
+                .createdAt(user.getCreatedAt())
+                .build();
+    }
+
+    public static UserResponseDTO.LoginResultDTO toUserLoginDTO() {
         return UserResponseDTO.LoginResultDTO.builder()
                 .userId(1L)
                 .build();
     }
 
-    public static UserResponseDTO.LogoutResultDTO toUserLogoutDTO(){
+    public static UserResponseDTO.LogoutResultDTO toUserLogoutDTO() {
         return UserResponseDTO.LogoutResultDTO.builder()
                 .userId(1L)
                 .build();
     }
 
-    public static UserResponseDTO.GetUserInfoDTO toUserGetInfoDTO(User user){
+    public static UserResponseDTO.GetUserInfoDTO toUserGetInfoDTO(User user) {
         return UserResponseDTO.GetUserInfoDTO.builder()
                 .nickname(user.getNickname())
                 .profile_image(user.getProfileImage())
@@ -33,20 +40,20 @@ public class UserConverter {
                 .build();
     }
 
-    public static UserResponseDTO.UpdateUserInfoDTO toUserUpdateDTO(User user){
+    public static UserResponseDTO.UpdateUserInfoDTO toUserUpdateDTO(User user) {
         return UserResponseDTO.UpdateUserInfoDTO.builder()
                 .userId(user.getUserId())
                 .build();
     }
 
-    public static UserResponseDTO.deleteUserDTO toUserDeleteDTO(){
+    public static UserResponseDTO.deleteUserDTO toUserDeleteDTO() {
         return UserResponseDTO.deleteUserDTO.builder()
                 .userId(1L)
                 .status(UserStatus.DELETED)
                 .build();
     }
 
-    public static UserResponseDTO.LoginResultDTO toUserAutoLoginDTO(){
+    public static UserResponseDTO.LoginResultDTO toUserAutoLoginDTO() {
         return UserResponseDTO.LoginResultDTO.builder()
                 .userId(1L)
                 .build();
