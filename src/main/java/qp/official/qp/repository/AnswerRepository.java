52bd0f670620fb4cf3a39b4c014a77f9b7a03c16
이원찬 <dnjscks4257@naver.com>
2024-01-24 12:26:15 +0900

    [feat] AnswerRepository 에서 Question 의 전문가 답변 갯수 조회 메서드

--- src/main/java/qp/official/qp/repository/AnswerRepository.java
+++ src/main/java/qp/official/qp/repository/AnswerRepository.java
@@ -1,7 +1,12 @@
 package qp.official.qp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import qp.official.qp.domain.Answer;
+import qp.official.qp.domain.Question;
+import qp.official.qp.domain.enums.Role;
 
+@Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    Integer countByQuestionAndUserRole(Question question, Role role);
 }
