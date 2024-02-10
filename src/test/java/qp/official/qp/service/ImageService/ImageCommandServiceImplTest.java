a51f7230fe0e47c5726a34ede4420e5e2aec9226
Minseok Kim <minseokkim@Minseokui-MacBookPro-2.local>
2024-02-10 23:47:12 +0900

    [test] LocalStackS3Config 클래스 생성

--- src/test/java/qp/official/qp/service/ImageService/ImageCommandServiceImplTest.java
+++ src/test/java/qp/official/qp/service/ImageService/ImageCommandServiceImplTest.java
@@ -1,7 +1,32 @@
 package qp.official.qp.service.ImageService;
 
+import static org.aspectj.weaver.tools.cache.SimpleCacheFactory.path;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import com.amazonaws.services.s3.AmazonS3Client;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import qp.official.qp.repository.ImageRepository;
 
+
+@ExtendWith(MockitoExtension.class)
 class ImageCommandServiceImplTest {
+
+    @Mock
+    private ImageRepository imageRepository;
+    @Mock
+    private AmazonS3Client amazonS3Client;
+    @InjectMocks
+    private ImageCommandServiceImpl imageCommandService;
 
 }
