a6455d60f985f29a856be7146fff3796e9b23e1c
이원찬 <dnjscks4257@naver.com>
2024-02-13 17:18:49 +0900

    [chore] Application Class 의사용하지 않는 import문 수정

--- src/main/java/qp/official/qp/QpBackendApplication.java
+++ src/main/java/qp/official/qp/QpBackendApplication.java
@@ -2,7 +2,6 @@ package qp.official.qp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
